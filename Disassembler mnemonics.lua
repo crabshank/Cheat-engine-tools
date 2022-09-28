595b43bca59160a3941c2e2dfdb20f5592810a26
crabshank <44329154+crabshank@users.noreply.github.com>
2022-09-28 11:38:28 +0100

    Add files via upload

---
+++ Disassembler mnemonics.lua
@@ -0,0 +1,16 @@
+-- Disassemblers mnemonics - crabshank
+
+local function displayMnemonics()
+  local visDis = getVisibleDisassembler()
+
+  function f(sender, address, LastDisassembleData, result, description)
+    if not sender.syntaxHighlighting then return end
+	setComment(LastDisassembleData.address,'〈' .. LastDisassembleData.description .. '〉 %s' )
+    return result,description
+  end
+
+  visDis.OnPostDisassemble = f
+
+end
+
+displayMnemonics()
