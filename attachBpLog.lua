5f4721cb8675c2d142bd8d1cb7ca1578df6aa664
crabshank <44329154+crabshank@users.noreply.github.com>
2022-12-18 14:03:41 +0000

    Add files via upload

--- attachBpLog.lua
+++ attachBpLog.lua
@@ -105,8 +105,10 @@ if abp ~=nil then
 				hexByteString = decByteString:gsub('%S+',function (c) return string.format('%02X',c) end)
 				regs[32+i]=hexByteString
 			end
-   dumpRegisters()
+
 			debug_continueFromBreakpoint(co_run)
+			else
+				 dumpRegisters()
 		end
 	end
 end
