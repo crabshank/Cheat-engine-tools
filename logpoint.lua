9fce7e9dc15f7b412459f97ecd8f1f94f0acd5a8
crabshank <44329154+crabshank@users.noreply.github.com>
2023-09-16 03:23:27 +0100

    Add files via upload

--- logpoint.lua
+++ logpoint.lua
@@ -315,7 +315,7 @@ local function dumpRegisters(k)
 			 if rivl >0 then
 				for i = 1, rivl do
 					if c==false then
-						print( ak['calcs'][i] ..' logged at ' .. ak['address_hex'] .. ' (' .. rivl .. ' hits):')
+						print( 'Logged at ' .. ak['address_hex'] .. ' (' .. rivl .. ' hits):')
 						c=true
 					end
 					if p~=true then
