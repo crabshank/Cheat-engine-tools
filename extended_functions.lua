ce971c97f69c283a0e2aa2181f912f430e02732b
crabshank <44329154+crabshank@users.noreply.github.com>
2024-02-01 11:30:06 +0000

    Add files via upload

--- extended_functions.lua
+++ extended_functions.lua
@@ -29,7 +29,7 @@ function enumModuleSymbols()
 	return out
 end
 
-local function table_filter(t,f)
+function table_filter(t,f)
 	local out={}
 	for k,v in pairs(t) do
 		local tyk=type(k)
@@ -44,7 +44,7 @@ local function table_filter(t,f)
 	return out
 end
 
-local function table_map(t,f) -- Javascript port
+function table_map(t,f) -- Javascript port
 	local out={}
 	for k,v in pairs(t) do
 		out[k]=f(v,k,t)
