bf0e532a6af36bb6ceb9ae816fbacb1969454475
crabshank <44329154+crabshank@users.noreply.github.com>
2024-04-23 23:03:25 +0100

    Add files via upload

--- maxScan.lua
+++ maxScan.lua
@@ -1,3 +1,4 @@
 MainForm.cbWritable.State=2
+MainForm.cbCopyOnWrite.State=2
 local hxl=string.len(MainForm.FromAddress.Text)
-MainForm.ToAddress.Text=string.rep('f',hxl)
+MainForm.ToAddress.Text='7'..string.rep('f',hxl-1)
